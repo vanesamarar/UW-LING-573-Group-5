50a476b6ffd672e0dec3f0fe29b12fa680b5ac8c
Nina Koh <ninabk14@gmail.com>
2025-04-23 13:49:49 -0700

    Created a Tokenizer & Stemmer
    the full list of ENGLISH_STOP_WORDS is here: https://github.com/scikit-learn/scikit-learn/blob/main/sklearn/feature_extraction/_stop_words.py

--- tf-idf.py
+++ tf-idf.py
@@ -2,12 +2,15 @@
 # Summarizes review data via Term Frequency-Inverse Document Frequency (TF-IDF)
 
 import os, glob, re, nltk
-from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
+from nltk.stem.snowball import SnowballStemmer
+from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS, TfidfVectorizer
 
 def load_data(data_dir):
     """Loads .txt.data files from the Opinosis dataset into a dictionary"""
     data = {} # maps topics to a list of reviews
     search_path = os.path.join(data_dir, "*.txt.data") # data/*.txt.data
+    # For testing on a small patch of data:
+    # search_path = os.path.join(data_dir, "price_amazon_kindle.txt.data")
     files = glob.glob(search_path) # files that match search pattern
     for file in files:
         filename = os.path.basename(file) # e.g. price_amazon_kindle.txt.data
@@ -30,10 +33,20 @@ def clean_line(line):
     return line
 
 def tokenize_line(line):
-    """Tokenizes the cleaned line & removes semantically-empty words (e.g., a, the)"""
+    """Tokenizes the cleaned line into a list & removes semantically-empty words (e.g., a, the)"""
     tokens = nltk.word_tokenize(line)
-    # remove stop words here
-    return tokens
+    filter_tokens = [token for token in tokens if token not in ENGLISH_STOP_WORDS]
+    return filter_tokens
+
+def stem_words(tokens):
+    """Stems the given tokens via an English-specific subclass (e.g., generously->generous)"""
+    stemmer = SnowballStemmer("english") # instance of english-specific stemmer
+    stems = [stemmer.stem(token) for token in tokens]
+    return stems
+
+def summarize_topic(n):
+    """Summarizes the top n sentences"""
 
 if __name__ == "__main__":
     data = load_data("data/") # loads data directly from data folder
+    # tokens = tokenize_line(line)
