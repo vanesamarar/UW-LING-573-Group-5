6232946722f4b79faf6ffdba5452f5853274a219
Nina Koh <ninabk14@gmail.com>
2025-06-02 22:16:17 -0700

    Create gold_info.py

---
+++ gold_info.py
@@ -0,0 +1,22 @@
+# Author: Nina Koh
+# This is a script to get quantitative informtion on the gold summaries
+
+# Comment on Deliverable 2: How long are the human-written summaries, on average?
+import os
+
+def get_paths(gold_dir):
+    """ """
+    paths = []
+    for topic in os.listdir(gold_dir): # generate list of topics (folders)
+        
+    
+    return paths
+
+def analyze_summaries():
+    """ """
+    
+    
+if __name__ == "__main__":
+    root_dir = 
+    gold_dir = 
+    paths = get_paths(gold_dir)
