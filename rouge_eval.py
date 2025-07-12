529d2798545d9f70bc2a0393d1552921eb41876d
Nina Koh <ninabk14@gmail.com>
2025-07-11 23:31:21 -0700

    Evaluate new trained summaries

--- rouge_eval.py
+++ rouge_eval.py
@@ -104,9 +104,10 @@ def print_rouge_scores(scores):
 
 if __name__ == "__main__":
     gold_dir = "summaries-gold"  
-    summary_file = "lexrank_summaries.json"   
+    # summary_file = "lexrank_summaries.json"   
     # summary_file = "tf-idf_results/tfidf_summaries.json"   
     # summary_file = "t5-small_results/t5_zero_shot_summaries.json" 
+    summary_file = "t5-small_results/t5_trained_summaries_new.json" # with topic mapping
     
     gold_summaries = load_gold_summaries(gold_dir)
     generated_summaries = load_generated_summaries(summary_file)
